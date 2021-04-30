9df02c8445eee9c68e118abc9626514e6e10a230
pauldeggington <pdeggington@gmail.com>
2021-04-29 18:37:37 -0700

    add animation

--- assets/js/text-loading.js
+++ assets/js/text-loading.js
@@ -1,151 +1,196 @@
-const HyperText = el => {
-	// Some configuration variables
-  const START = 0;
-  const END = 35;
-  const STAGGER = 1.65;
-  const RANDOM_CHAR = 0.25;
-  const LINE_DELAY = 100;
+const hypertext = (function() {
+    function countLines(elem) {
+        let lineHeight = window.getComputedStyle(elem, null).getPropertyValue('line-height');
 
-	// Internal Text handler
-  const _HyperText = el => {
-    let frame = 0;
+        //     Box Height          Line Height
+        return elem.offsetHeight / parseInt(lineHeight);
+    }
 
-    const randomChar = () => {
-      const chars = '!<>-_\\/[]{}—=+*^?#-----------------------------------------------------------------------------------------------';
+    function getLines(el) {
+        let lineCount = countLines(el);
+        let html = el.innerHTML.replace(/(\S+\s*)/g, '<span>$1</span>');
 
-      return chars[Math.floor(Math.random() * chars.length)]
-    };
+        el.innerHTML = html;
 
-    const getScript = text => {
-      return text.split('').reduce((arr, char, indx) => {
-        const start = Math.floor(Math.random() * START) + (indx * STAGGER);
-        const stop = start + Math.floor(Math.random() * END);
+        var offset = 0;
+        var spans = document.querySelectorAll('.hypertext span');
 
-        arr.push({
-          char,
-          start,
-          stop
+        function getLine(index) {
+            var top = 0,
+                buffer = [];
+
+            for (var i = 0; i < spans.length; i++) {
+                if (top > index) {
+                    break;
+                }
+
+                var newOffset = spans[i].offsetTop;
+
+                if (newOffset !== offset) {
+                    offset = newOffset;
+                    top++;
+                }
+
+                if (top === index) {
+                    buffer.push(spans[i]);
+                }
+            }
+
+            var text = [];
+
+            for (var i = 0; i < buffer.length; i++) {
+                text.push(buffer[i]);
+            }
+
+            return text;
+        }
+
+        var lines = [];
+
+        for (var i = 0; i < lineCount; i++) {
+            lines.push(getLine(i + 1));
+        }
+
+        return lines;
+    }
+
+    function replaceAt(str, index, char) {
+        return str.substr(0, index) + char + str.substr(index + char.length);
+    }
+
+    let probability = function(n) {
+        return !!n && (Math.random() * 10) <= n;
+    }
+
+    function hypertextChar(char) {
+        return probability(0.65) ? '/' : '-';
+    }
+
+    // -------------------------------------------------------------------
+
+    function editChar(el, text) {
+        let index = 0;
+
+        const step = setInterval(() => {
+            if (text[index] === undefined) return;
+            
+            el.innerText = replaceAt(el.innerText, index, text[index]);
+            index++;
+
+            // If this was the last one, clear the interval
+            if (index == text.length) clearInterval(step);
+        }, 50);
+    }
+
+    function animateChar(el, word) {
+        let text = word.innerText.replace(/\s/g, '');
+        let index = 0;
+
+        // RESET
+        el.innerText = '';
+
+        const step = setInterval(() => {
+            el.innerText += hypertextChar(text[index]);
+            index++;
+
+            // If this was the last one, clear the interval
+            if (index == text.length) {
+                clearInterval(step);
+                editChar(el, text);
+            }
+        }, 50);
+    }
+
+    function animateLine(args) {
+        let { clone, data, index } = args;
+
+        const row = clone.querySelector('.ht-row:nth-of-type(' + index + ')');
+
+        // Start at 0
+        let i = 0;
+
+        const step = setInterval(() => {
+            let span = document.createElement('span');
+            span.classList.add('ht-word');
+            
+            row.insertAdjacentHTML('beforeend', String.fromCharCode(160));
+            row.insertAdjacentElement('beforeend', span);
+
+            // ---------------------------------------------------------
+
+            animateChar(span, data[i]);
+            i++;
+
+            if (i == data.length) clearInterval(step);
+        }, 130);
+    }
+
+    function _renderText(el, settings) {
+        // Create a new element based off the 'el' nodeName (eg. 'p')
+        const clone = document.createElement(el.nodeName);
+        clone.classList.add('hypertext-clone');
+
+        const lines = getLines(el);
+
+        el.style.display = 'none';
+        el.insertAdjacentElement('afterend', clone);
+
+        // -------------------------------------------------------------------------
+
+        let i = lines.length;
+
+        while (i--) {
+            let div = document.createElement('div');
+            div.classList.add('ht-row');
+
+            // TODO: find a better way give .ht-row it's height
+            // 	     at the moment a &nbsp is being used... eww
+            div.innerHTML = '&nbsp;';
+
+            clone.insertAdjacentElement('beforeend', div);
+        }
+
+        // -------------------------------------------------------------------------
+
+        let timing = -200;
+        let index = 0;
+
+        let animatedLines = lines.map((data) => {
+            timing += 200;
+
+            return setTimeout(() => {
+                index++;
+                return animateLine({
+                    clone,
+                    data,
+                    index
+                });
+            }, timing);
         });
+    }
 
-        return arr;
-      }, []);
+    // Handler function
+    function init(config) {
+        const defaults = {
+            'selector': '.hypertext'
+        };
+
+        const settings = Object.assign(defaults, config);
+
+        // ----------------------------------------------------
+
+        const elements = document.querySelectorAll(settings.selector);
+
+        return Object.keys(elements).map((key) => {
+            return _renderText(elements[key]);
+        });
+    }
+
+    return {
+        init
     };
+})();
 
-    const animate = args => {
-      let complete = 0;
-
-      let {
-        el,
-        scripts,
-        frame
-      } = args;
-
-      let output = scripts.reduce((html, script, indx) => {
-        let {
-          char,
-          start,
-          stop,
-          currentChar
-        } = script;
-
-        if (frame >= stop) {
-          complete++;
-          html += char;
-        } else if (frame >= start) {
-          if (!currentChar || Math.random() < RANDOM_CHAR) {
-            currentChar = randomChar();
-            scripts[indx].currentChar = currentChar;
-          }
-
-          html += `<span class='d'>${currentChar}</span>`;
-        } else {
-          html += '';
-        }
-
-        return html;
-      }, '');
-
-      el.innerHTML = output;
-
-      if (complete === scripts.length) {
-        cancelAnimationFrame(raf);
-      } else {
-        frame++;
-        raf = requestAnimationFrame(() => animate({
-          el,
-          scripts,
-          frame
-        }))
-      }
-    };
-
-    const scripts = getScript(el.innerText);
-
-    el.classList.add('active');
-
-    let raf = requestAnimationFrame(() => animate({
-      el,
-      scripts,
-      frame
-    }));
-
-    return true;
-  }
-
-  const getEachLine = text => {
-    let newText = text.innerHTML.replace(/(\S+\s*)/g, '<span>$1</span>');
-    text.innerHTML = newText;
-
-    let spans = text.querySelectorAll('span');
-
-    const consolidateWordsIntoLine = words => {
-      let container = document.createElement('span');
-      container.classList.add('text-wrap');
-
-      return Array.from(words).reduce((line, span) => {
-        line.innerText += span.innerText;
-
-        return line;
-      }, container);
-    };
-
-    const groupWordsByOffset = words => {
-      return Array.from(words).reduce((map, word) => {
-        let offset = word.offsetTop;
-
-        if (!map.has(offset)) {
-          map.set(offset, []);
-        }
-
-        map.get(offset).push(word);
-
-        return map;
-      }, new Map());
-    };
-
-    let wordMap = groupWordsByOffset(spans);
-
-    text.innerHTML = '';
-
-    wordMap.forEach(row => {
-      let line = consolidateWordsIntoLine(row);
-
-      text.appendChild(line)
-    });
-
-    return document.querySelectorAll('.text-wrap');
-  };
-
-  const eachLine = getEachLine(el);
-  
-  let delay = 0;
-  
-  return Array.from(eachLine).map(line => {
-		delay += LINE_DELAY;
-    return setTimeout(() => _HyperText(line), delay);
-  });
-}
-
-let text = document.querySelector('.text');
-
-HyperText(text);
+hypertext.init({
+    'selector': '.hypertext'
+});
